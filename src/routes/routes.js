61c216d7bd818e16e65e168533e1dd21a9675de5
Tanapoom <tanapoom0347@gmail.com>
2023-09-10 20:04:10 +0700

    api

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -2,6 +2,6 @@ module.exports = app => {
     const user123Router = require('./users123sample/router');
     const std001Router = require('./students123sample/router');
 
-    app.use('/user123', user123Router);
-    app.use('/std001', std001Router);
+    app.use('/api/user123', user123Router);
+    app.use('/api/std001', std001Router);
 };
