6d6b7c0f51bd60277536a7b790d99d5d9df814cf
Tanapoom <tanapoom0347@gmail.com>
2023-09-15 03:44:54 +0700

    fifth

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.set('json spaces', 2);
 
 require('./src/routes/routes')(app);
 
