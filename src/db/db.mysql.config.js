e492b2b14367ce7b3b881e5b09a742a089ac80af
Tanapoom <tanapoom0347@gmail.com>
2023-09-15 18:07:22 +0700

    edit

--- src/db/db.mysql.config.js
+++ src/db/db.mysql.config.js
@@ -4,7 +4,7 @@ const connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
     password: 'admin',
-    database: 'test'
+    database: 'api_db'
   });
 
 module.exports = connection;
